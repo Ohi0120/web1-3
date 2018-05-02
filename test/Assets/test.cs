55c651850210e0d06f50a817f44f222fa8863813
Unknown <g1727056@st.t-kougei.ac.jp>
2018-05-02 18:18:26 +0900

    大人のモビット

---
+++ test/Assets/test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+        // Hello,Worldをコンソールウィンドウに表示する
+        Debug.Log("Hello,World");
+        int age;
+        age = 30;
+        Debug.Log(age);
+        float height1 = 160.5f;
+        float height2;
+        height2 = height1;
+        Debug.Log(height2);
+        string name;
+        name = "kitamura";
+        Debug.Log(name);
+        int answer;
+        answer = 1 + 2;
+        Debug.Log(answer);
+        int n1 = 8;
+        int n2 = 9;
+        answer = n1 + n2;
+        Debug.Log(answer);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
